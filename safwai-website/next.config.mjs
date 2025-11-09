dac0a99fb58a072a9a2a06494fe15b7553407606
jeroenatdoha <jeroenatdoha@outlook.com>
2025-11-09 13:45:17 +0300

    Update next.config.mjs

--- safwai-website/next.config.mjs
+++ safwai-website/next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  {
   output: 'export',
   images: {
     unoptimized: true,
-  }
+  },
 }
 
 export default nextConfig
